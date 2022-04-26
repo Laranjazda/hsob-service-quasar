702eb49cd6b0bfbd1cd2af7f4eb40c909230d807
laranjazda <carloseng.jr@gmail.com>
2022-04-25 23:45:30 -0300

    firt commit

---
+++ src/layouts/FormUser.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="q-pa-md">
+    <div class="q-gutter-md" style="max-width: 300px">
+      <q-input outlined v-model="text" label="Name" />
+    </div>
+  </div>
+</template>
+
+<style></style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      class="logo"
+    />
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+});
+</script>
+
+<style scoped>
+.logo {
+  align-items: center;
+  width: 200px;
+  height: 200px;
+  border: red 2px solid;
+}
+</style>
+>
